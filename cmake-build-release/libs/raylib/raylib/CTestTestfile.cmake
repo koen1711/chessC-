ad2144f2bdaad6f14619bb063c37e37155746d8c
koen1711 <za4266468@gmail.com>
2023-08-28 21:27:54 +0200

    Remove build dirs

--- cmake-build-release/libs/raylib/raylib/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /media/koen/PortableSSD/GitHub/ChessC++/libs/raylib/src
-# Build directory: /media/koen/PortableSSD/GitHub/ChessC++/cmake-build-release/libs/raylib/raylib
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("external/glfw")
